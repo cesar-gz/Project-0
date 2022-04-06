db270d14eb2f7ddba543988eb8a1631be765dcb8
ChristianjTeodoro <christianjte@csu.fullerton.edu>
2022-04-06 09:05:06 -0700

    backend edit

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -4,7 +4,8 @@ export default class RestaurantsController{
     static async apiGetRestaurants( req, res, next) {
         const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20
         const page = req.query.page ? parseInt(req.query.page, 10) : 0
-
+        
+        //query
         let filters = {}
         if (req.query.cuisine){
             filters.cuisine = req.query.cuisine
