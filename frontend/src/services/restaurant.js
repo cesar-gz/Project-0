0e76180bc690e8eb32e6a53038b24668cb91dc41
Cesar Gutierrez <cesargutierrezceja@gmail.com>
2022-04-18 15:46:54 -0700

    switched to nodemon server

--- frontend/src/services/restaurant.js
+++ frontend/src/services/restaurant.js
@@ -4,10 +4,10 @@ class RestaurantDataService {
   getAll(page = 0) {
     
     // this one is for nodemon server
-    //return http.get(`?page=${page}`);
+    return http.get(`?page=${page}`);
 
     // this one is for mongoDB
-    return http.get(`restaurants?page=${page}`);
+    //return http.get(`restaurants?page=${page}`);
   }
 
   get(id) {
@@ -16,46 +16,46 @@ class RestaurantDataService {
     //return http.get(`/id/${id}`);
 
     // this one is active with MongoDb
-    return http.get(`/restaurant/id/${id}`);
+    //return http.get(`/restaurant/id/${id}`);
     
     // this one works with nodemon server
-    //return http.get(`/id/${id}`);
+    return http.get(`/id/${id}`);
   }
 
   find(query, by = "name", page = 0) {
 
     // this one is for nodemon server
-    //return http.get(`?${by}=${query}&page=${page}`);
+    return http.get(`?${by}=${query}&page=${page}`);
     
     // this one is for mongoDB
-    return http.get(`restaurants?${by}=${query}&page=${page}`);
+    //return http.get(`restaurants?${by}=${query}&page=${page}`);
   } 
 
   createReview(data) {
 
     // nodemon servers
-    // return http.post("/review", data);
+    return http.post("/review", data);
     
     // mongoDB
-    return http.post("/review-new", data);
+    //return http.post("/review-new", data);
   }
 
   updateReview(data) {
     
     // nodemon servers
-    //return http.put("/review", data);
+    return http.put("/review", data);
     
     // mongoDB
-    return http.put("/review-edit", data);
+    //return http.put("/review-edit", data);
   }
 
   deleteReview(id, userId) {
     
     // nodemon servers
-    //return http.delete(`/review?id=${id}`, {data:{user_id: userId}});
+    return http.delete(`/review?id=${id}`, {data:{user_id: userId}});
   
     // mongoDB
-    return http.delete(`/review-delete?id=${id}`, {data:{user_id: userId}});
+    //return http.delete(`/review-delete?id=${id}`, {data:{user_id: userId}});
   }
 
   getCuisines(id) {
