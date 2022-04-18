f3d8553d3dc678d820ebc69211aa9c45d5f20255
MORGAN_LUCAS <LUCAS.MORGAN567@CSU.FULLERTON.EDU>
2022-04-18 14:29:59 -0700

    the skeleton of the spinwheel page, needs love

---
+++ frontend/src/components/spinwheel.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import RestaurantDataService from "../services/restaurant";
+import { Link } from "react-router-dom";
+
+const Spinwheel = props => {
+
+//creating an empty list which will be filled fom the restaurants the users choose 
+ let spinwheel = [];
+ 
+//need to create a spinwheel and link the different sections of the spinwheel to elements in the list that will grow as the user adds restaurants 
+
+
+
+
+};
+
+export default Spinwheel;
